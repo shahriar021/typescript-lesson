47b612808bc8c8012ea278e6fa89a47573d117ec
shahriar021 <shahriarc19@gmail.com>
2024-01-14 20:44:46 +0600

    added something from home

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,8 @@
-var uName:string|number;
-uName ="777"
-uName=777
-let some = "sdf"
+var uName: string | number;
+uName = "777";
+uName = 777;
+let some = "sdf";
 
-var color ="black"
+var color = "black";
+
+var changedGITlocation = 4561;
